2ad955b913fbc71a62ac8cf160f62c42dc6a2e24
liuyujie-c <71921328+liuyujie-c@users.noreply.github.com>
2020-10-29 17:52:12 +0800

    Update settings.py

--- Boss/settings.py
+++ Boss/settings.py
@@ -87,3 +87,4 @@ ITEM_PIPELINES = {
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+LOG_FILE = "books.log"
